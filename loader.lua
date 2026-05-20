98e33fccc9cc5a55b7da832641b9908b3f95b786
nothm_ <127741557+not-hm@users.noreply.github.com>
2026-05-20 18:23:07 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,6 @@
 repeat task.wait() until game:IsLoaded()
 if game.PlaceId == 11630038968 or game.PlaceId == 10810646982 or game.PlaceId == 139566161526375 then
-	--loadstring(game:HttpGet("https://raw.githubusercontent.com/not-hm/LimeForRoblox/refs/heads/main/bridge_duel.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/not-hm/sigeon.beta/refs/heads/main/bridge_duel.lua"))()
 elseif game.PlaceId == 71480482338212 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/not-hm/sigeon.beta/refs/heads/main/bed_fight.lua"))()
 else
